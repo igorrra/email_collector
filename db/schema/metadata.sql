7ac60247c0a56dba6a0d2bc1723be1f12db3360d
Igor Yanushevskyi <igorrra@gmail.com>
2018-06-11 13:01:00 +0300

    Changed sql commands formatting and added utf8mb4 character set support

--- db/schema/metadata.sql
+++ db/schema/metadata.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS `email_collector`.`metadata` (
 	`id`		INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`sender`	VARCHAR(256) NOT NULL COMMENT 'The sender of the email data object',
 	`subject`	VARCHAR(256) COMMENT 'The subject of the email data object',
-	`body`		TEXT COMMENT 'Body (text/plain) of the email data object',
-	`html`		TEXT COMMENT 'Body (text/html) of the email data object',
+	`body`		LONGTEXT COMMENT 'Body (text/plain) of the email data object',
+	`html`		LONGTEXT COMMENT 'Body (text/html) of the email data object',
 	`timestamp`	INT(11) NOT NULL COMMENT 'Timestamp that message was received at',
 	PRIMARY KEY (`id`),
 	INDEX `sender_idx` (`sender` ASC),
 	INDEX `subject_idx` (`subject` ASC)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE = utf8mb4_general_ci;
--- lib/database.py
+++ lib/database.py
@@ -11,7 +11,7 @@ from lib.decorators import db_connection_wrapper
 def read(db_connection, data_id=None):
     """Show report with all data by joining all tables."""
     cmd = 'SELECT m.id id, sender, recipient, subject, body, ' \
-          'html, timestamp, attachment_name, content_type, ' \
+          'timestamp, attachment_name, content_type, ' \
           'path, md5 ' \
           'FROM metadata m ' \
           'LEFT JOIN attachments a ON m.id=a.metadata_id ' \
@@ -34,41 +34,47 @@ def read(db_connection, data_id=None):
 def post(db_connection, params):
     """Insert posted data into database."""
     cur = db_connection.cursor()
+
+    cur.execute('SET NAMES utf8mb4')
+    cur.execute("SET CHARACTER SET utf8mb4")
+    cur.execute("SET character_set_connection=utf8mb4")
+
     metadata_cmd = 'INSERT INTO metadata ' \
                    '(sender, subject, body, html, timestamp) ' \
-                   'VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\')' \
-                   % (params.get('from'), params.get('subject'),
-                      params.get('body'), params.get('html'),
-                      params.get('timestamp'))
-    cur.execute(metadata_cmd)
+                   'VALUES (%s, %s, %s, %s, %s)'
+    cur.execute(metadata_cmd,
+                (params.get('from'), params.get('subject'),
+                 params.get('body'), params.get('html'),
+                 params.get('timestamp'))
+                )
 
     last_id = db_connection.insert_id()
 
     for recipient in params.get('to'):
         recipients_cmd = 'INSERT INTO recipients ' \
                           '(recipient, metadata_id) ' \
-                          'VALUES (\'%s\', \'%s\')' \
-                          % (recipient, last_id)
-        cur.execute(recipients_cmd)
+                          'VALUES (%s, %s)'
+        cur.execute(recipients_cmd, (recipient, last_id))
 
     for attachment in params.get('attachments'):
         attachments_cmd = 'INSERT INTO attachments ' \
               '(attachment_name, content_type, md5, path, metadata_id) ' \
-              'VALUES (\'%s\', \'%s\',\'%s\', \'%s\', \'%s\')' \
-              % (attachment.name, attachment.content_type,
-                 attachment.md5, attachment.path, last_id)
-        cur.execute(attachments_cmd)
+              'VALUES (%s, %s, %s, %s, %s)'
+        cur.execute(attachments_cmd,
+                    (attachment.name, attachment.content_type,
+                     attachment.md5, attachment.path, last_id)
+                    )
 
     return 'Affected rows: %s' % (cur.rowcount, )
 
 
 @db_connection_wrapper
-def delete(db_connection, table_name, data_id):
+def delete(db_connection, data_id):
     """Delete data with specified id from specified table."""
-    delete_cmd = 'DELETE FROM %s ' \
-                 'WHERE id=%s' % (table_name, data_id)
     cur = db_connection.cursor()
-    cur.execute(delete_cmd)
+    delete_cmd = 'DELETE FROM metadata ' \
+                 'WHERE id=%s'
+    cur.execute(delete_cmd, (data_id, ))
 
     return 'Affected rows: %s' % (cur.rowcount, )
 
@@ -79,8 +85,8 @@ def put(db_connection, data_id, params):
     cur = db_connection.cursor()
     for key in params.keys():
         put_cmd = 'UPDATE metadata m ' \
-                  'JOIN attachments a ON m.id=a.metadata_id ' \
-                  'JOIN  recipients r ON m.id=r.metadata_id ' \
+                  'LEFT JOIN attachments a ON m.id=a.metadata_id ' \
+                  'LEFT JOIN  recipients r ON m.id=r.metadata_id ' \
                   'SET %s="%s" WHERE m.id="%s";' \
                   % (key, params['%s' % key], data_id)
         cur.execute(put_cmd)
