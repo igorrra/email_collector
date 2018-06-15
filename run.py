db8ed379911a3c386a5c0d4a89578c5fb0831f78
Igor Yanushevskyi <igorrra@gmail.com>
2018-06-15 11:05:43 +0300

    Basic HTTPAuth and possibility download file objects added

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ from ConfigParser import ConfigParser
 from flask import Flask
 from flask import flash, jsonify, make_response, redirect, request, send_file
 
+from flask_httpauth import HTTPBasicAuth
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 from utils.email_parser import parse_raw_email
@@ -42,6 +43,8 @@ app.config['MYSQL_DATABASE_PASSWORD'] = config.get('mysql', 'password')
 app.config['MYSQL_DATABASE_DB'] = config.get('mysql', 'db')
 app.config['MYSQL_DATABASE_HOST'] = config.get('mysql', 'host')
 
+auth = HTTPBasicAuth()
+
 mysql = MySQL(cursorclass=DictCursor)
 mysql.init_app(app)
 
@@ -49,13 +52,8 @@ logging.config.fileConfig(CONFIG_PATH)
 logger = logging.getLogger(__name__)
 
 
-def allowed_file(filename):
-    """Check if uploaded file extension is allowed."""
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EMAIL_EXTENSIONS
-
-
 @app.route('/api/v1/email', methods=['GET', 'POST'])
+@auth.login_required
 def upload_email():
     """Create new entries in database by parsing uploaded emails."""
     db = mysql.connect()
@@ -97,6 +95,7 @@ def upload_email():
 
 
 @app.route('/api/v1/email/all', methods=['GET'])
+@auth.login_required
 def read_email():
     """Show contents of all joined tables."""
     db = mysql.connect()
@@ -107,6 +106,7 @@ def read_email():
 
 @app.route('/api/v1/email/<int:metadata_id>',
            methods=['GET', 'DELETE', 'PUT'])
+@auth.login_required
 def work_with_email_by_id(metadata_id):
     """Show contents of all joined tables."""
     db = mysql.connect()
@@ -124,22 +124,53 @@ def work_with_email_by_id(metadata_id):
         return make_response(jsonify({'Response': result}), st_code)
 
 
-@app.route('/api/v1/attachment/<path>')
-def download_attachment(path=None):
-    if not path:
-        return make_response(
-            jsonify({'Response': 'Path was not specified'}),
-            400)
-    try:
-        return send_file(path, as_attachment=True)
-    except Exception as e:
-        logger.exception(e)
+@app.route('/api/v1/email/attachments/<directory>/<filename>')
+@auth.login_required
+def download_attachment(directory=None, filename=None):
+    """Download attachment from server."""
+    path = 'attachments/' + directory + '/' + filename
+
+    if request.method == 'GET':
+        try:
+            if os.path.exists(path):
+                logger.debug('Download attachment %s called', path)
+                return send_file(path, as_attachment=True)
+
+            logger.debug('Nonexistent path specified %s', path)
+            return make_response(
+                jsonify({'Response': "Selected path '%s' "
+                                     "does not exist" % path}), 400)
+        except Exception as error:
+            logger.exception(error)
+            return make_response(
+                jsonify({'Response': "Selected path '%s' "
+                                     "does not exist" % path}), 400)
 
 
 @app.errorhandler(404)
 def not_found(error):
     """Not found error."""
     return make_response(jsonify({'Error': 'Not found'}), 404)
+
+
+@auth.get_password
+def get_password(username):
+    """Basic authorization."""
+    if username == config.get('flask', 'user'):
+        return config.get('flask', 'password')
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    """Unauthorized error."""
+    return make_response(jsonify({'Error': 'Unauthorized access'}), 401)
+
+
+def allowed_file(filename):
+    """Check if uploaded file extension is allowed."""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EMAIL_EXTENSIONS
 
 
 def parse_args():
